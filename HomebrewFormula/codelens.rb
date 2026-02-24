7e9693941534ce1cdbb5af6865a38671936e5caa
Kevin Aubrée <kev.aubree@gmail.com>
2026-02-24 22:33:21 +0100

    feat: add Homebrew formula template

---
+++ HomebrewFormula/codelens.rb
@@ -0,0 +1,35 @@
+class Codelens < Formula
+  desc "Agentic memory & semantic search MCP server for Claude Code"
+  homepage "https://github.com/MakFly/codelens-v2"
+  version "0.2.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/MakFly/codelens-v2/releases/download/v0.2.0/codelens_0.2.0_darwin_arm64.tar.gz"
+      sha256 "TODO"
+    else
+      url "https://github.com/MakFly/codelens-v2/releases/download/v0.2.0/codelens_0.2.0_darwin_amd64.tar.gz"
+      sha256 "TODO"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/MakFly/codelens-v2/releases/download/v0.2.0/codelens_0.2.0_linux_arm64.tar.gz"
+      sha256 "TODO"
+    else
+      url "https://github.com/MakFly/codelens-v2/releases/download/v0.2.0/codelens_0.2.0_linux_amd64.tar.gz"
+      sha256 "TODO"
+    end
+  end
+
+  def install
+    bin.install "codelens"
+    bin.install "codelens-hook" if OS.mac?
+  end
+
+  test do
+    assert_match "CodeLens", shell_output("#{bin}/codelens version")
+  end
+end
